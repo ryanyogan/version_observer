c8f54c6a4fae1dff1a084cbab5f6e48821b42150
ryan <ryanyogan@gmail.com>
2021-07-09 09:26:39 -0500

    libcluster (GOSSIP) & Horde configured for multiple node distribution

--- mix.exs
+++ mix.exs
@@ -14,9 +14,6 @@ defmodule VersionObserver.MixProject do
     ]
   end
 
-  # Configuration for the OTP application.
-  #
-  # Type `mix help compile.app` for more information.
   def application do
     [
       mod: {VersionObserver.Application, []},
@@ -24,13 +21,9 @@ defmodule VersionObserver.MixProject do
     ]
   end
 
-  # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
-  # Specifies your project dependencies.
-  #
-  # Type `mix help deps` for examples and options.
   defp deps do
     [
       {:phoenix, "~> 1.5.9"},
@@ -39,16 +32,12 @@ defmodule VersionObserver.MixProject do
       {:telemetry_metrics, "~> 0.4"},
       {:telemetry_poller, "~> 0.4"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:libcluster, "~> 3.3"},
+      {:horde, "~> 0.8.3"}
     ]
   end
 
-  # Aliases are shortcuts or tasks specific to the current project.
-  # For example, to install project dependencies and perform other setup tasks, run:
-  #
-  #     $ mix setup
-  #
-  # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
       setup: ["deps.get", "cmd npm install --prefix assets"]
